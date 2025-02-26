19c84c91263290c89885809f08e8b448ec687b5e
Sumbal Naz <Codeq209@gmail.com>
2025-02-26 21:40:13 +0500

    Update index.py

--- Step07-Python and Its Language Connections/index.py
+++ Step07-Python and Its Language Connections/index.py
@@ -3,19 +3,14 @@ print(bin(10))
 
 #Assembly Language
 import ctypes
-print(ctypes.c_int(10).value)  # Output: 10
-
+print(ctypes.c_int(10).value) 
 
 # #C/C++ (Low-Level Languages)
 import ctypes
 libc = ctypes.CDLL("msvcrt.dll")  
 libc.printf(b"Hello from C!\n")
 
-
-
 # #SQL (Database Language)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 print("Database Connected!")
-
-#HTML & JavaScript (Web Development)
