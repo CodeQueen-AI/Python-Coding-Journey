8b0cab3e3083ba70c52c3371ca72bc205e590aca
Sumbal Naz <Codeq209@gmail.com>
2025-02-26 21:42:36 +0500

    Update index.py

--- Step08-Syntax/index.py
+++ Step08-Syntax/index.py
@@ -1,9 +1,9 @@
 #Indentation
 if True:
-    print("Indented correctly")  # ✅ 
+    print("Indented correctly") 
     
 # if True:
-# print("This will give an error!")   ❌ 
+# print("This will give an error!")   
 
 #Case Sensitive
 name = "Alice"
@@ -12,36 +12,36 @@ Name = "Bob"
 print(Name) 
 
 #Comments
-print("Hello, Python!")  # Yeh print hoga, magar comment ignore hoga
+print("Hello, Python!") 
 
 """
-Yeh aik multi-line comment hai.
-Jo multiple lines mei likha ja sakta hai.
+This is a multi-line comment
+It can be written in multiple lines
 """
 print("Python is fun!")
 
 #End of Statement
-print("Hello, Python!")  # ✅ 
-#print("Hello"); print("Python") ❌ 
+print("Hello, Python!")
+#print("Hello"); print("Python") 
 
 #Printing Output print() function
 print("Hello, World!")
 
 #User Input
-name = input("What's Your Name: ")
-print("Hello,", name)
+name = input("What is Your Name: ")
+print("Hello!", name)
 
 #Variable Declaration
-age = 25      # Integer
+age = 25      
 print(age)
-name = "Ali"  # String
+name = "Ali" 
 print(name)
-price = 99.99 # Float
+price = 99.99 
 print(price)
 
 #Line Continuation
 total = (10 + 20 +
-         30 + 40)  # Parentheses ka istemal
+         30 + 40)
 print(total)
 message = "Yeh aik lambi string hai " \
           "jo multiple lines pe likhi gayi hai"
