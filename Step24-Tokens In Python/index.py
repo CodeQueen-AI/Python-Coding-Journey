411334f7d0a669a976a3bc73748197f799c512fe
Sumbal Naz <Codeq209@gmail.com>
2025-03-07 15:49:30 +0500

    Add files via upload

---
+++ Step24-Tokens In Python/index.py
@@ -0,0 +1,20 @@
+# Keywords
+if True:
+    print("This is a keyword example")  
+
+# Identifiers
+my_var = 10 
+print(my_var)
+
+# Literals
+num = 100  
+text = "Hello"  
+print(num , text)
+
+# Operators
+sum_result = 5 + 3  
+print(sum_result)
+
+# Punctuators
+def greet():  
+    print("Hello!")
