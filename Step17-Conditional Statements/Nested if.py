128715761cf6b1442a36259dcb77c3264c946b9f
Sumbal Naz <Codeq209@gmail.com>
2025-03-16 20:31:51 +0500

    Update Nested if.py

--- Step17-Conditional Statements/Nested if.py
+++ Step17-Conditional Statements/Nested if.py
@@ -3,3 +3,9 @@ if num > 0:
     print("Positive Number")
 if num % 2 == 0:
     print("Even Number")
+
+age = 20  
+if age >= 18: 
+    print("You are eligible to vote.")  
+if age >= 21:
+    print("You are also eligible to apply for a driving license.")
