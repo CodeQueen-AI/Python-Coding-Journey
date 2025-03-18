3b40b015382d99becb5930868e3c3990b66dc230
Sumbal Naz <Codeq209@gmail.com>
2025-03-18 20:08:53 +0500

    Add files via upload

---
+++ Step27-Number System/index.py
@@ -0,0 +1,21 @@
+#Decimal 
+num = 10  
+print(num)  
+
+#Binary
+num = 0b1010  
+print(num) 
+
+#Octal
+num = 0o12  
+print(num) 
+
+#Hexadecimal
+num = 0xA  
+print(num)  
+
+#convert a number from one system to another in Python, we can use built-in functions
+num = 10
+print(bin(num))  
+print(oct(num))  
+print(hex(num)) 
