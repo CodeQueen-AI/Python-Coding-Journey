785f51cd1493a6574a3ef9df11dc14a670b48561
Sumbal Naz <Codeq209@gmail.com>
2025-03-18 20:57:12 +0500

    Add files via upload

---
+++ Step28-Rules for Naming Conventions/index.py
@@ -0,0 +1,32 @@
+#PascalCase
+name1 = 'CodeQueen'
+
+#camelCase
+name2 = 'codeQueen'
+
+# snake_case
+name3 = 'code_queen'
+
+# kebab-case
+name4 = 'code-queen'
+
+# flatcase
+name5 = 'codequeen'
+
+# UPPERFLATCASE
+name6 = 'CODEQUEEN'
+
+# Pascal_Snake_Case
+name7 = 'Code_Queen_Python'
+
+# camel_Snake_Case
+name8 = 'code_Queen_Python'
+
+# SCREAMING_SNAKE_CASE
+name9 = 'CODE_QUEEN_PYTHON'
+
+# Train-Case
+name10 = 'Code-Queen'
+
+# COBOL-CASE
+name11 = 'CODE-QUEEN'
