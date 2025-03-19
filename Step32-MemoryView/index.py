658982b141e86ee74349efce966e81dc0ada7f08
Sumbal Naz <Codeq209@gmail.com>
2025-03-19 16:42:55 +0500

    Add files via upload

---
+++ Step32-MemoryView/index.py
@@ -0,0 +1,17 @@
+#Creating a MemoryView
+data = bytearray("Hello", "utf-8")  
+data1 = memoryview(data)  
+print(data1[0]) 
+print(data1[:])  
+
+#Modifying Data Without Copying 
+data = bytearray("Hello", "utf-8")
+data1 = memoryview(data)
+data1[0] = 74  
+print(data)  
+
+#Slicing in MemoryView
+data = bytearray("Python", "utf-8")
+data1 = memoryview(data)
+print(data1[2:])  
+print(data1[2:].tobytes())  
