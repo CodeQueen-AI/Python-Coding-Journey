b5fbf9a80d3b2dc67269682459369d855af149d1
Sumbal Naz <Codeq209@gmail.com>
2025-03-19 14:20:46 +0500

    Add files via upload

---
+++ Step29-Encoding & Character Handling/index.py
@@ -0,0 +1,15 @@
+#Unicode Characters
+print("\u0939\u0947\u0932\u094b") 
+print("\u0633\u0644\u0627\u0645")  
+
+#ASCII
+print(ord('A'))
+print(chr(97))  
+
+#UTF-8
+text = "Hello, 你好, مرحبا"
+encoded_text = text.encode("utf-8")  
+print(encoded_text)  
+decoded_text = encoded_text.decode("utf-8")
+print(decoded_text)  
+
