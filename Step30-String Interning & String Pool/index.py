24276d6b616ffa61955ba0072053e4426bba453e
Sumbal Naz <Codeq209@gmail.com>
2025-03-19 14:51:09 +0500

    Add files via upload

---
+++ Step30-String Interning & String Pool/index.py
@@ -0,0 +1,15 @@
+#String Pool
+s1 = "hello"
+s2 = "hello"
+print(s1 is s2)  
+
+#String Interning
+import sys
+s1 = sys.intern("world")
+s2 = sys.intern("world")
+print(s1 is s2)  
+
+
+s1 = "python"
+s2 = "".join(["py", "thon"])  
+print(s1 is s2) 
