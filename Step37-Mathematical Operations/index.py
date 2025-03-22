83c34bfd84ecd5256371d1a65bc3e443e8cd196d
Sumbal Naz <Codeq209@gmail.com>
2025-03-22 14:46:45 +0500

    Add files via upload

---
+++ Step37-Mathematical Operations/index.py
@@ -0,0 +1,32 @@
+#Basic Mathematical Functions
+import math
+
+print(math.sqrt(25))   
+print(math.pow(2, 3))  
+print(math.ceil(4.3))  
+print(math.floor(4.9))
+
+
+#Trigonometric Functions
+import math
+
+angle = math.radians(30)  
+
+print(math.sin(angle)) 
+print(math.cos(angle))
+print(math.tan(angle))
+
+#Logarithmic & Exponential Functions
+import math
+
+print(math.log(10))  
+print(math.log10(100))
+print(math.exp(2))   
+
+
+#Constants in Math Module
+import math
+
+print(math.pi)  
+print(math.e)    
+print(math.inf) 
