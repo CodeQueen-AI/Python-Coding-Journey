9c754aaaa37aeacdc304f487519473afcc128b27
Sumbal Naz <Codeq209@gmail.com>
2025-04-07 00:53:46 +0500

    Update Accessing Elements.py

--- Step22-Tuples/Accessing Elements.py
+++ Step22-Tuples/Accessing Elements.py
@@ -5,7 +5,7 @@ print(my_tuple[2])
 
 # Negative Indexing
 print(my_tuple[-1])
-print(my_tuple[-2])  
+print(my_tuple[-2])
 
 # Slicing
 my_tuple = ("apple", "banana", "cherry", "date", "elderberry")
