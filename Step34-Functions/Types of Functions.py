404561a619bc2d8365a7ede0293566a558f1a7b2
Sumbal Naz <Codeq209@gmail.com>
2025-04-08 12:38:50 +0500

    Update Types of Functions.py

--- Step34-Functions/Types of Functions.py
+++ Step34-Functions/Types of Functions.py
@@ -10,7 +10,7 @@ print(square(5))
 
 #Arrow Functions
 square = lambda x: x * x  
-print(square(5))  
+print(square(5))
 
 #Immediately Invoked Function Expressions (IIFE)
 (lambda x: print(f"Hello, {x}!"))("CodeQueen")
