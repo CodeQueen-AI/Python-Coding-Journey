efb1f7cc213dd272a026c15937d967aff00dc619
Sumbal Naz <Codeq209@gmail.com>
2025-04-10 19:37:54 +0500

    Update Overloading.py

--- Step34-Functions/Overloading.py
+++ Step34-Functions/Overloading.py
@@ -1,7 +1,7 @@
 class Example:
     def show(self, *args):
         if len(args) == 1:
-            print(f"One parameter: {args[0]}")
+            print(f"One parameters {args[0]}")
         elif len(args) == 2:
             print(f"Two parameters: {args[0]}, {args[1]}")
         else:
