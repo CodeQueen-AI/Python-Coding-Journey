9547fbfe12993004cf44174f51a5eb78ae0a2608
Sumbal Naz <Codeq209@gmail.com>
2025-04-12 11:24:56 +0500

    Update Dic Unpacking.py

--- Step21-Dictionary/Dic Unpacking.py
+++ Step21-Dictionary/Dic Unpacking.py
@@ -1,11 +1,10 @@
 # Dictionary Unpacking in a Function
 def show_details(name, age):
     print(f"Name: {name}, Age: {age}")
-
 person = {"name": "Code Queen", "age": 22}
 show_details(**person)  
 
-#  Merging Dictionaries
+#Merging Dictionaries
 dict1 = {"a": 1, "b": 2}
 dict2 = {"c": 3, "d": 4}
 merged_dict = {**dict1, **dict2} 
