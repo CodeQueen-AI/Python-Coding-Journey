b3c1510c98eb6f7334d71d0ecfe78d1100e5cda7
Sumbal Naz <Codeq209@gmail.com>
2025-04-16 09:51:54 +0500

    Update Loop with Else Statement.py

--- Step25-Loops/Loop with Else Statement.py
+++ Step25-Loops/Loop with Else Statement.py
@@ -3,7 +3,6 @@ for num in range(1, 6):
 else:  
     print("Loop completed successfully!")  
 
-
 #If a loop stops with a break statement, the else block does not execute
 for num in range(1, 6):  
     if num == 3:  
