f08666fc5bc5f397151efc5ed050b06d3b67ce6f
Sumbal Naz <Codeq209@gmail.com>
2025-04-19 22:45:23 +0500

    Update index.py

--- Step07-Python and Its Language Connections/index.py
+++ Step07-Python and Its Language Connections/index.py
@@ -1,15 +1,12 @@
 # Binary Language
 print(bin(10)) 
-
 #Assembly Language
 import ctypes
 print(ctypes.c_int(10).value)
-
 # #C/C++ (Low-Level Languages)
 import ctypes
 libc = ctypes.CDLL("msvcrt.dll")  
 libc.printf(b"Hello from C!\n")
-
 # #SQL (Database Language)
 import sqlite3
 conn = sqlite3.connect(":memory:")
