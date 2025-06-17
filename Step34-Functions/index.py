8c0fcfe4e3283f9d2ba66430bfa7d56f44c3b5e9
Sumbal Naz <Codeq209@gmail.com>
2025-06-17 15:29:47 +0500

    Update index.py

--- Step34-Functions/index.py
+++ Step34-Functions/index.py
@@ -1,6 +1,11 @@
-#Defining a Function
-def greet():
-    print("Hello, Code Queen!")  
+#Simple Functions
 
-#Calling a Function
-greet()
+#Initialize
+def message():
+    print('Hello I am CodeQueen, Future AI Engineer')
+#calling
+message()
+
+def add_number():
+    print(5 + 5)
+add_number()
